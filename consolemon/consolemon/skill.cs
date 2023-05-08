05f19253e719b51eedeb655a832041650ac832fe
Owen Stas <34884@ma-web.nl>
2023-05-08 12:02:57 +0100

    3&3/5

--- consolemon/consolemon/skill.cs
+++ consolemon/consolemon/skill.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace consolemon
 {
-    internal class skill
+    internal class Skill
     {
         int Damage;
         int Energycost;
         string Name;
+        public Skill(int damage, int engerycost, string name)
+        {
+            this.Damage = damage;
+            this.Energycost = engerycost;
+            this.Name = name;
+        }
+        internal void Useon(string target, string caster)
+        {
+            //mon.DepleteEnergy();
+        }
+
     }
 }
