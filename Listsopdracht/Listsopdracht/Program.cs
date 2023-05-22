6afaa56c6203a3864287a238e765383daf82d2fa
owen22s <34884@ma-web.nl>
2023-05-22 11:05:51 +0100

    test

---
+++ Listsopdracht/Listsopdracht/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ListOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<string> birds = new List<string>() { };
+            birds.Add("uil");
+            birds.Add("kraai");
+            birds.Add("hond");
+            birds.Add("papegaai");
+
+            List<string> birds2 = new List<string>() { };
+            birds2.Add("meeuw");
+            birds2.Add("duif");
+
+            birds.AddRange(birds2);
+
+            birds.Remove("hond");
+
+            for (int i = 0; i < birds.Count; i++)
+            {
+                Console.WriteLine(birds[i]);
+            }
+        }
+    }
+}
