5e5da87db2ab44b308c1ebe7957b786d48784ac3
owen22s <34884@ma-web.nl>
2023-06-05 10:57:13 +0100

    12

--- consolemon/consolemon/consolemonfactory.cs
+++ consolemon/consolemon/consolemonfactory.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
 
 internal class ConsoleMonFactory
 {
@@ -10,19 +12,15 @@ internal class ConsoleMonFactory
         {
             Console.WriteLine(line);
         }
-        
+
         string filePath = "monsterdata.txt";
         string fileContent = File.ReadAllText(filePath);
 
-        
         string[] fileLines = fileContent.Split('|');
 
-        
         for (int i = 0; i < fileLines.Length; i++)
         {
-            
             string[] typeSplit = fileLines[i].Split('|');
-            
             string[] consoleMonData = typeSplit[0].Split(',');
             string waarde1 = consoleMonData[0];
             string waarde2 = consoleMonData[1];
@@ -32,9 +30,47 @@ internal class ConsoleMonFactory
             Console.WriteLine("Waarde 2: " + waarde2);
             Console.WriteLine("Waarde 3: " + waarde3);
             Console.WriteLine("Waarde 4: " + waarde4);
-
         }
+    }
 
+    internal void LoadJson(string datafile)
+    {
+        string json = File.ReadAllText(datafile);
+        Console.WriteLine(json);
+
+        List<ConsoleMon> templates = JsonSerializer.Deserialize<List<ConsoleMon>>(json);
+        Console.WriteLine(templates[0].name);
     }
 }
 
+public class Skill
+{
+    public string name { get; set; }
+    public int power { get; set; }
+    public int cost { get; set; }
+}
+
+public class ConsoleMon
+{
+    public string name { get; set; }
+    public int health { get; set; }
+    public int attack { get; set; }
+    public int defense { get; set; }
+    public List<Skill> skills { get; set; }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        TestFactoryFunctions();
+    }
+
+    static void TestFactoryFunctions()
+    {
+        Console.WriteLine("TestFactoryFunctions");
+        ConsoleMonFactory factory = new ConsoleMonFactory();
+        factory.Load("monsterdata.txt");
+        factory.LoadJson("monsterdata.json");
+    }
+}
